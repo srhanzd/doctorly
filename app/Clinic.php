489d6c6ede13e439a9265c42625d391fba89f05a
srhanzd <srhanzd@gmail.com>
2022-08-07 21:30:12 +0300

    c2

---
+++ app/Clinic.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use App\Doctor;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clinic extends Model
+{
+    use HasFactory;
+    protected $table = 'clinics';
+    protected $fillable = [
+        'id','name','description','created_at','updated_at'];
+    protected $hidden =[
+    ];
+
+    public function doctor(){
+        return $this->hasMany(Doctor::class,'clinic_id','id');
+    }
+
+//    public function services(){
+//        return $this->hasMany(Service::class,'clinic_id','id');
+//    }
+//
+//    public function review(){
+//        return $this->hasMany(Review::class,'clinic_id','id');
+//    }
+//
+//    public function reservation(){
+//        return $this->hasMany(Reservation::class,'clinic_id','id');
+//    }
+//
+//    public function offer(){
+//        return $this->hasMany(Offer::class,'clinic_id','id');
+//    }
+}
