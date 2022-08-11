f2f8c6d01a8e33668802149ae82556afa85a703f
srhanzd <srhanzd@gmail.com>
2022-08-11 05:50:15 +0300

    final1

--- resources/views/layouts/admin-dashboard.blade.php
+++ resources/views/layouts/admin-dashboard.blade.php
@@ -5,7 +5,7 @@
             <h4 class="mb-0 font-size-18">{{ __('Dashboard') }}</h4>
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item active">Welcome to {{ config('app.name') }} Dashboard</li>
+                    <li class="breadcrumb-item active"> Pro Touch Dashboard</li>
                 </ol>
             </div>
         </div>
@@ -20,7 +20,7 @@
                     <div class="col-7">
                         <div class="text-primary p-3">
                             <h5 class="text-primary">{{ __('Welcome Back !') }}</h5>
-                            <p>{{ config('app.name') }} Dashboard</p>
+                            <p> Pro Touch Dashboard</p>
                         </div>
                     </div>
                     <div class="col-5 align-self-end">
@@ -47,7 +47,7 @@
                                     <a href="{{ url('/doctor') }}" class="mb-0 font-weight-medium font-size-15">
                                         <h5 class="mb-0">{{ number_format($data['total_doctors']) }}</h5>
                                     </a>
-                                    <p class="text-muted mb-0">Clinics</p>
+                                    <p class="text-muted mb-0">Doctors</p>
                                 </div>
                                 <div class="col-6">
                                     <a href="{{ url('/patient') }}" class="mb-0 font-weight-medium font-size-15">
@@ -71,67 +71,67 @@
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div clas6s="card-body">
-                <h4 class="card-title mb-4">{{ __('Monthly Earning') }}</h4>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <p class="text-muted">{{ __('This month') }}</p>
-                        <h3>${{ number_format($data['monthly_earning']) }}</h3>
-                        <p class="text-muted">
-                            <span class="@if ($data['monthly_diff'] > 0) text-success @else text-danger @endif mr-2">
-                                {{ $data['monthly_diff'] }}% <i class="mdi @if ($data['monthly_diff'] > 0) mdi-arrow-up @else mdi-arrow-down @endif"></i>
-                            </span>{{ __('From previous month') }}
-                        </p>
-                    </div>
-                    <div class="col-sm-6">
-                        <div id="radialBar-chart" class="apex-charts"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-8">
-            <div class="card mini-stats-wid">
-                <div class="card-body">
-                    <div class="media">
-                        @if (session()->has('page_limit'))
-                            @php
-                                $per_page = session()->get('page_limit');
-                            @endphp
-                        @else
-                            @php
-                                $per_page = Config::get('app.page_limit');
-                            @endphp
-                        @endif
-                        <div class="media-body">
-                            <p class="text-muted font-weight-medium">Display items per page</p>
-                            <button
-                                class="btn  {{ $per_page == 10 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"
-                                data-page="10">10</button>
-                            <button
-                                class="btn  {{ $per_page == 25 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"
-                                data-page="25">25</button>
-                            <button
-                                class="btn  {{ $per_page == 50 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"
-                                data-page="50">50</button>
-                            <button
-                                class="btn  {{ $per_page == 100 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"
-                                data-page="100">100</button>
-                        </div>
-                        <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                            <span class="avatar-title rounded-circle bg-primary">
-                                <i class="bx bx-book-open font-size-24"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="card">--}}
+{{--            <div clas6s="card-body">--}}
+{{--                <h4 class="card-title mb-4">{{ __('Monthly Earning') }}</h4>--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-sm-6">--}}
+{{--                        <p class="text-muted">{{ __('This month') }}</p>--}}
+{{--                        <h3>${{ number_format($data['monthly_earning']) }}</h3>--}}
+{{--                        <p class="text-muted">--}}
+{{--                            <span class="@if ($data['monthly_diff'] > 0) text-success @else text-danger @endif mr-2">--}}
+{{--                                {{ $data['monthly_diff'] }}% <i class="mdi @if ($data['monthly_diff'] > 0) mdi-arrow-up @else mdi-arrow-down @endif"></i>--}}
+{{--                            </span>{{ __('From previous month') }}--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-sm-6">--}}
+{{--                        <div id="radialBar-chart" class="apex-charts"></div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="d-md-inline">--}}
+{{--            <div class="card mini-stats-wid">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="media">--}}
+{{--                        @if (session()->has('page_limit'))--}}
+{{--                            @php--}}
+{{--                                $per_page = session()->get('page_limit');--}}
+{{--                            @endphp--}}
+{{--                        @else--}}
+{{--                            @php--}}
+{{--                                $per_page = Config::get('app.page_limit');--}}
+{{--                            @endphp--}}
+{{--                        @endif--}}
+{{--                        <div class="media-body">--}}
+{{--                            <p class="text-muted font-weight-medium">Display items per page</p>--}}
+{{--                            <button--}}
+{{--                                class="btn  {{ $per_page == 10 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"--}}
+{{--                                data-page="10">10</button>--}}
+{{--                            <button--}}
+{{--                                class="btn  {{ $per_page == 25 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"--}}
+{{--                                data-page="25">25</button>--}}
+{{--                            <button--}}
+{{--                                class="btn  {{ $per_page == 50 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"--}}
+{{--                                data-page="50">50</button>--}}
+{{--                            <button--}}
+{{--                                class="btn  {{ $per_page == 100 ? 'btn-primary' : 'btn-info' }}  btn-sm mr-2 per-page-items  mb-md-1"--}}
+{{--                                data-page="100">100</button>--}}
+{{--                        </div>--}}
+{{--                        <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">--}}
+{{--                            <span class="avatar-title rounded-circle bg-primary">--}}
+{{--                                <i class="bx bx-book-open font-size-24"></i>--}}
+{{--                            </span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 
-    <div class="col-xl-8">
+    <div class="col-8">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-12">
                 <div class="card mini-stats-wid">
                     <div class="card-body">
                         <div class="media">
@@ -148,41 +148,41 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card mini-stats-wid">
-                    <div class="card-body">
-                        <div class="media">
-                            <div class="media-body">
-                                <p class="text-muted font-weight-medium">{{ __('Revenue') }}</p>
-                                <h4 class="mb-0">${{ number_format($data['revenue'], 2) }}</h4>
-                            </div>
-                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                                <span class="avatar-title rounded-circle bg-primary">
-                                    <i class="bx bx-dollar font-size-24"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mini-stats-wid">
-                    <div class="card-body">
-                        <div class="media">
-                            <div class="media-body">
-                                <p class="text-muted font-weight-medium">{{ __("Today's Earning") }}</p>
-                                <h4 class="mb-0">${{ number_format($data['daily_earning'], 2) }}</h4>
-                            </div>
-                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                                <span class="avatar-title rounded-circle bg-primary">
-                                    <i class="bx bxs-dollar-circle  font-size-24"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
+{{--            <div class="col-md-4">--}}
+{{--                <div class="card mini-stats-wid">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <div class="media">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <p class="text-muted font-weight-medium">{{ __('Revenue') }}</p>--}}
+{{--                                <h4 class="mb-0">${{ number_format($data['revenue'], 2) }}</h4>--}}
+{{--                            </div>--}}
+{{--                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">--}}
+{{--                                <span class="avatar-title rounded-circle bg-primary">--}}
+{{--                                    <i class="bx bx-dollar font-size-24"></i>--}}
+{{--                                </span>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-4">--}}
+{{--                <div class="card mini-stats-wid">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <div class="media">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <p class="text-muted font-weight-medium">{{ __("Today's Earning") }}</p>--}}
+{{--                                <h4 class="mb-0">${{ number_format($data['daily_earning'], 2) }}</h4>--}}
+{{--                            </div>--}}
+{{--                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">--}}
+{{--                                <span class="avatar-title rounded-circle bg-primary">--}}
+{{--                                    <i class="bx bxs-dollar-circle  font-size-24"></i>--}}
+{{--                                </span>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <div class="col-6">
                 <div class="card mini-stats-wid">
                     <div class="card-body">
                         <div class="media">
@@ -202,7 +202,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-6">
                 <div class="card mini-stats-wid">
                     <div class="card-body">
                         <div class="media">
@@ -219,37 +219,37 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card mini-stats-wid">
-                    <div class="card-body">
-                        <div class="media">
-                            <div class="media-body">
-                                <p class="text-muted font-weight-medium">{{ __('Upcoming Appointments') }}</p>
-                                <a href="{{ url('/upcoming-appointment') }}"
-                                    class="mb-0 font-weight-medium font-size-24">
-                                    <h4 class="mb-0">{{ number_format($data['Upcoming_appointment']) }}
-                                    </h4>
-                                </a>
-                            </div>
-                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                                <span class="avatar-title rounded-circle bg-primary">
-                                    <i class='bx bxs-calendar-minus font-size-24'></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="col-md-4">--}}
+{{--                <div class="card mini-stats-wid">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <div class="media">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <p class="text-muted font-weight-medium">{{ __('Upcoming Appointments') }}</p>--}}
+{{--                                <a href="{{ url('/upcoming-appointment') }}"--}}
+{{--                                    class="mb-0 font-weight-medium font-size-24">--}}
+{{--                                    <h4 class="mb-0">{{ number_format($data['Upcoming_appointment']) }}--}}
+{{--                                    </h4>--}}
+{{--                                </a>--}}
+{{--                            </div>--}}
+{{--                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">--}}
+{{--                                <span class="avatar-title rounded-circle bg-primary">--}}
+{{--                                    <i class='bx bxs-calendar-minus font-size-24'></i>--}}
+{{--                                </span>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
         </div>
 
         <!-- end row -->
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title mb-4">{{ __('Monthly Registered Users') }}</h4>
-                <div id="monthly_users" class="apex-charts" dir="ltr"></div>
-            </div>
-        </div>
+{{--        <div class="card">--}}
+{{--            <div class="card-body">--}}
+{{--                <h4 class="card-title mb-4">{{ __('Monthly Registered Users') }}</h4>--}}
+{{--                <div id="monthly_users" class="apex-charts" dir="ltr"></div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </div>
 <!-- end row -->
@@ -288,29 +288,31 @@
                                     <tr>
                                         <th>{{ __('Sr.No.') }}</th>
                                         <th>{{ __('Name') }}</th>
-                                        <th>{{ __('Degree') }}</th>
+{{--                                        <th>{{ __('Degree') }}</th>--}}
                                         <th>{{ __('Contact No') }}</th>
                                         <th>{{ __('Email') }}</th>
                                         <th>{{ __('View Details') }}</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($clinics as $item)
-                                        <tr>
-                                            <td>{{ $loop->index + 1 }}</td>
-                                            <td>{{ $item->name }}
-                                                <!-- Button trigger modal -->
-                                            <!--   <a href="{{ url('doctor/' . $item->id) }}">
-                                                    <button type="button"
+                                @foreach ($doctors as $item)
+                                    <tr>
+                                        <td>{{ $loop->index + 1 }}</td>
+                                        <td>{{ $item->first_name }} {{ $item->last_name }}</td>
+{{--                                        <td>{{ $item->doctor->degree }}</td>--}}
+                                        <td>{{ $item->mobile }}</td>
+                                        <td>{{ $item->email }}</td>
+                                        <td>
+                                            <!-- Button trigger modal -->
+                                            <a href="{{ url('doctor/' . $item->id) }}">
+                                                <button type="button"
                                                         class="btn btn-primary btn-sm btn-rounded waves-effect waves-light">
-                                                        {{ __('View Details') }}
-
-                                                    </button>
-                                                </a>
-                                                -->
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                                    {{ __('View Details') }}
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
--- resources/views/layouts/top-hor.blade.php
+++ resources/views/layouts/top-hor.blade.php
@@ -16,7 +16,7 @@
                         <img src="{{ URL::asset('assets/images/logo-light.png') }}" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ URL::asset('assets/images/logo-light1.png') }}" alt="" height="19">
+                        <img src="{{ URL::asset('assets/images/logo-light1.png') }}" alt="" height="30">
                     </span>
                 </a>
             </div>
@@ -25,57 +25,57 @@
                 <i class="fa fa-fw fa-bars"></i>
             </button>
         </div>
+{{--        <div class="d-flex">--}}
+{{--            <div class="dropdown d-inline-block d-lg-none ml-2">--}}
+{{--                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"--}}
+{{--                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                    <i class="mdi mdi-magnify"></i>--}}
+{{--                </button>--}}
+{{--                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"--}}
+{{--                    aria-labelledby="page-header-search-dropdown">--}}
+{{--                    <form class="p-3">--}}
+{{--                        <div class="form-group m-0">--}}
+{{--                            <div class="input-group">--}}
+{{--                                <input type="text" class="form-control" placeholder="{{ __("Search ...") }}"--}}
+{{--                                    aria-label="Recipient's username">--}}
+{{--                                <div class="input-group-append">--}}
+{{--                                    <button class="btn btn-primary" type="submit"><i--}}
+{{--                                            class="mdi mdi-magnify"></i></button>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <!-- App Search-->--}}
+{{--        </div>--}}
         <div class="d-flex">
             <div class="dropdown d-inline-block d-lg-none ml-2">
-                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="mdi mdi-magnify"></i>
-                </button>
+{{--                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"--}}
+{{--                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                    <i class="mdi mdi-magnify"></i>--}}
+{{--                </button>--}}
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
                     aria-labelledby="page-header-search-dropdown">
-                    <form class="p-3">
-                        <div class="form-group m-0">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="{{ __("Search ...") }}"
-                                    aria-label="Recipient's username">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="submit"><i
-                                            class="mdi mdi-magnify"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+{{--                    <form class="p-3">--}}
+{{--                        <div class="form-group m-0">--}}
+{{--                            <div class="input-group">--}}
+{{--                                <input type="text" class="form-control" placeholder="{{ __("Search ...") }}"--}}
+{{--                                    aria-label="Recipient's username">--}}
+{{--                                <div class="input-group-append">--}}
+{{--                                    <button class="btn btn-primary" type="submit"><i--}}
+{{--                                            class="mdi mdi-magnify"></i></button>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
                 </div>
             </div>
-            <!-- App Search-->
-        </div>
-        <div class="d-flex">
-            <div class="dropdown d-inline-block d-lg-none ml-2">
-                <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="mdi mdi-magnify"></i>
-                </button>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
-                    aria-labelledby="page-header-search-dropdown">
-                    <form class="p-3">
-                        <div class="form-group m-0">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="{{ __("Search ...") }}"
-                                    aria-label="Recipient's username">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="submit"><i
-                                            class="mdi mdi-magnify"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="dropdown d-none d-lg-inline-block ml-1">
-                <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
-                    <i class="bx bx-fullscreen"></i>
-                </button>
-            </div>
+{{--            <div class="dropdown d-none d-lg-inline-block ml-1">--}}
+{{--                <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">--}}
+{{--                    <i class="bx bx-fullscreen"></i>--}}
+{{--                </button>--}}
+{{--            </div>--}}
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item noti-icon waves-effect"
                     id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true"
@@ -142,8 +142,8 @@
                     @elseif($role == 'admin')
                     <a class="dropdown-item" href="{{ url('profile-edit') }}"><i
                         class="bx bx-user font-size-16 align-middle mr-1"></i> {{ __("Change Profile") }}</a>
-                    <a class="dropdown-item" href="{{ url('payment-key') }}"><i
-                        class="bx bx-key font-size-16 align-middle mr-1"></i> {{ __("Add Api Key") }}</a>
+{{--                    <a class="dropdown-item" href="{{ url('payment-key') }}"><i--}}
+{{--                        class="bx bx-key font-size-16 align-middle mr-1"></i> {{ __("Add Api Key") }}</a>--}}
                     @endif
                     <a class="dropdown-item d-block" href="{{ url('change-password') }}"><i
                             class="bx bx-wrench font-size-16 align-middle mr-1"></i> {{ __("Change Password") }}</a>
@@ -157,11 +157,11 @@
                     </form>
                 </div>
             </div>
-            <div class="dropdown d-inline-block">
-                <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect ">
-                    <i class="bx bx-cog bx-spin"></i>
-                </button>
-            </div>
+{{--            <div class="dropdown d-inline-block">--}}
+{{--                <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect ">--}}
+{{--                    <i class="bx bx-cog bx-spin"></i>--}}
+{{--                </button>--}}
+{{--            </div>--}}
         </div>
     </div>
 </header>
